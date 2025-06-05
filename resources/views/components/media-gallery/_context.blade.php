c5666899c2e41ac2490ceb6d7aac7848766bcc30
Tjalling Feenstra <hello@tjalling.net>
2025-06-05 21:08:58 +0200

    fix asset urls

--- resources/views/components/media-gallery/_context.blade.php
+++ resources/views/components/media-gallery/_context.blade.php
@@ -1,6 +1,6 @@
-<script src="{{ asset('vendor/tjall/media-gallery/js/modal/MediaModal.js') }}" defer></script>
-<script src="{{ asset('vendor/tjall/media-gallery/js/modal/MediaModalItem.js') }}" defer></script>
-<script src="{{ asset('vendor/tjall/media-gallery/js/modal/MediaModalContent.js') }}" defer></script>
+<script src="{{ asset('vendor/tjall/laravel-mediagallery/js/modal/MediaModal.js') }}" defer></script>
+<script src="{{ asset('vendor/tjall/laravel-mediagallery/js/modal/MediaModalItem.js') }}" defer></script>
+<script src="{{ asset('vendor/tjall/laravel-mediagallery/js/modal/MediaModalContent.js') }}" defer></script>
 
 <div x-data="{ modal: new MediaModal() }">
     {{ $slot }}
