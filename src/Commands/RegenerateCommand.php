39b2b2f9e2c745a52dbcefffa746572583a23a5b
Tjalling Feenstra <hello@tjalling.net>
2025-07-29 22:44:03 +0200

    fix: regenerate command signature

--- src/Commands/RegenerateCommand.php
+++ src/Commands/RegenerateCommand.php
@@ -4,5 +4,12 @@
     use Spatie\MediaLibrary\Conversions\Commands\RegenerateCommand as SpatieRegenerateCommand;
 
     class RegenerateCommand extends SpatieRegenerateCommand {
-        protected $signature = 'media-gallery:regenerate';
+        protected $signature = 'media-gallery:regenerate {modelType?} {--ids=*}
+        {--only=* : Regenerate specific conversions}
+        {--starting-from-id= : Regenerate media with an id equal to or higher than the provided value}
+        {--X|exclude-starting-id : Exclude the provided id when regenerating from a specific id}
+        {--only-missing : Regenerate only missing conversions}
+        {--with-responsive-images : Regenerate responsive images}
+        {--force : Force the operation to run when in production}
+        {--queue-all : Queue all conversions, even non-queued ones}';
     }
