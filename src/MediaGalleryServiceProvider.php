e46ef4f69cfdff94ca4fb1a10c16888505a74fd4
Tjalling Feenstra <hello@tjalling.net>
2025-07-29 22:44:12 +0200

    fix: add media_model to config

--- src/MediaGalleryServiceProvider.php
+++ src/MediaGalleryServiceProvider.php
@@ -9,6 +9,7 @@ use Tjall\MediaGallery\Commands\RegenerateCommand;
 use Illuminate\Support\Facades\Config;
 use Tjall\MediaGallery\Support\HashedPathGenerator;
 use Tjall\MediaGallery\Support\HashedFileNamer;
+use Tjall\MediaGallery\Models\MediaItem;
 
 class MediaGalleryServiceProvider extends PackageServiceProvider {
     public function configurePackage(Package $package): void {
@@ -28,7 +29,8 @@ class MediaGalleryServiceProvider extends PackageServiceProvider {
         $this->publishes([
             __DIR__.'/../resources/js' => public_path('vendor/tjall/laravel-mediagallery/js'),
         ], 'laravel-assets');
-
+        
+        Config::set('media-library.media_model', MediaItem::class);
         Config::set('media-library.disk_name', config('media-gallery.disk_name', 'public'));
         Config::set('media-library.path_generator', HashedPathGenerator::class);
         Config::set('media-library.file_namer', HashedFileNamer::class);
